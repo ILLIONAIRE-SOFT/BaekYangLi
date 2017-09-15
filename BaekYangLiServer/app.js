febd826cd868fe8dc2a09d5ce2c8740b585c00e9
jkl5616 <jkl5616@gmail.com>
2017-09-16 01:02:38 +0900

    add getNearStations

--- BaekYangLiServer/app.js
+++ BaekYangLiServer/app.js
@@ -3,6 +3,10 @@ var express = require('express')
   , app = express()
   , server = http.createServer(app);
 var config = require(process.cwd()+'/db/config.js');
+var mysql = require('mysql');
+var connection = mysql.createConnection(config.db);
+connection.connect();
+
 var request = require('sync-request');
 
 app.get('/', function (req, res) {
@@ -15,6 +19,13 @@ app.get('/world.html', function (req, res) {
 
 app.get('/getArrivalTimeOfStation/:station_code', function (req, res) {
   res.send(getArrivalTimeOfStation(req.params.station_code));
+});
+
+app.get('/getNearStations/:lat/:lng', function(req, res) {
+  var sql = "SELECT *, (lat-"+req.params.lat+")*(lat-"+req.params.lat+")+(lng-"+req.params.lng+")*(lng-"+req.params.lng+") AS D from station order by D limit 5;";
+  connection.query(sql, function(err, result) {
+    res.send(result);
+  });
 });
 
 server.listen(8000, function() {
