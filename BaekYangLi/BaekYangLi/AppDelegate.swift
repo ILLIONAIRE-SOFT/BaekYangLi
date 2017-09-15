481e13c057b5b33f1a5f5afdb78b863f00ae4b95
sohn126 <nhos126@gmail.com>
2017-09-16 01:30:52 +0900

    add folders

--- BaekYangLi/BaekYangLi/AppDelegate.swift
+++ BaekYangLi/BaekYangLi/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // Tab2 테스트할때 아래 initViewController를 Tab2로
         
-        let initViewController = "Main"
+        let initViewController = "Tab2"
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
