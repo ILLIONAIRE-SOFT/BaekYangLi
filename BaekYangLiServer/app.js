619db3af6aa1fe3f12d89369a9667b94babe7e27
jkl5616 <jkl5616@gmail.com>
2017-09-16 00:53:37 +0900

    add getArrivalTimeOfStation:

--- BaekYangLiServer/app.js
+++ BaekYangLiServer/app.js
@@ -2,6 +2,8 @@ var express = require('express')
   , http = require('http')
   , app = express()
   , server = http.createServer(app);
+var config = require(process.cwd()+'/db/config.js');
+var request = require('sync-request');
 
 app.get('/', function (req, res) {
   res.send('Hello /');
@@ -11,6 +13,16 @@ app.get('/world.html', function (req, res) {
   res.send('Hello World');
 });
 
+app.get('/getArrivalTimeOfStation/:station_code', function (req, res) {
+  res.send(getArrivalTimeOfStation(req.params.station_code));
+});
+
 server.listen(8000, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
+function getArrivalTimeOfStation(stationCode) {
+  var res = request('GET', 'http://openapi.seoul.go.kr:8088/'+config.key+'/json/SearchArrivalTimeOfLine2SubwayByFRCodeService/1/5/'+stationCode+'/1/1/');
+  var data = JSON.parse(res.getBody('utf8'));
+  return data.SearchArrivalTimeOfLine2SubwayByFRCodeService.row;
+}
