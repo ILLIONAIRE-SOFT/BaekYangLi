6b4fc1afde0a7253729036bb3c33c22a98067746
jkl5616 <jkl5616@gmail.com>
2017-09-16 02:49:36 +0900

    fix

--- BaekYangLiServer/app.js
+++ BaekYangLiServer/app.js
@@ -48,7 +48,7 @@ app.get('/getNearStations/:lat/:lng', function(req, res) {
 });
 
 app.get('/getStationsByLine/:line', function(req, res) {
-  var sql = "SELECT *, (SELECT COUNT(*) from station where name = A.name ) AS transfer from station AS A where line = " + req.params.line;
+  var sql = "SELECT *, (SELECT COUNT(*) from station where name = A.name ) AS transfer from station AS A where line = \"" + req.params.line+"\"";
   console.log(sql);
   connection.query(sql, function(err, result) {
     res.send(result);
