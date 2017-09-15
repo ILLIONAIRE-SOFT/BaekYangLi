f53d30f6632cc0a940780517fc1d862c9074f606
LeFal <qwertyhj2@gmail.com>
2017-09-16 00:49:28 +0900

    divide StoryBoard

--- BaekYangLi/BaekYangLi/AppDelegate.swift
+++ BaekYangLi/BaekYangLi/AppDelegate.swift
@@ -16,7 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Tab2 테스트할때 아래 initViewController를 Tab2로
+        
+        let initViewController = "Main"
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: initViewController, bundle: nil)
+        
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: initViewController)
+        self.window?.rootViewController = initialViewController
+        
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
