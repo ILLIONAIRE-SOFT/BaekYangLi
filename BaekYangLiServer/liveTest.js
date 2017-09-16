e0aed3a50132be2016ddf834366e33cc1188fb01
jkl5616 <jkl5616@gmail.com>
2017-09-16 12:33:20 +0900

    test

--- BaekYangLiServer/liveTest.js
+++ BaekYangLiServer/liveTest.js
@@ -1,5 +1,5 @@
 var svg = d3.select("body").select("svg#map");
-var path = svg.select("path#path");
+var path = svg.select("path#path2");
 var startPoint = pathStartPoint(path);
 //Get path start point for placing marker
 function pathStartPoint(path) {
@@ -10,24 +10,24 @@ function pathStartPoint(path) {
   
   function transition() {
     marker.transition()
-        .duration(7500)
-        .attrTween("transform", translateAlong(path.node()))
+        .duration(10000)
+        .attrTween("transform", translateAlong(path.node(),0))
         .each("end", transition);// infinite loop
   }
   
-  function translateAlong(path) {
+  function translateAlong(path, reverse) {
     var l = path.getTotalLength();
     return function(i) {
       return function(t) {
-        var p = path.getPointAtLength(t * l);
-        return "translate(" + p.x + "," + p.y + ")";//Move marker
+        var p = path.getPointAtLength(reverse == 1 ? l - t * l : t * l);
+        return "translate(" + (p.x-5) + "," + (p.y-5) + ")";//Move marker
       }
     }
   }
 
 
-var marker = svg.append("circle");
-marker.attr("r", 7)
-  .attr("transform", "translate(" + startPoint + ")");
+var marker = svg.append("rect");
+marker.attr("width", 10).attr("height",10)
+  .attr("transform", "translate(" + startPoint + ")").attr("fill","#038fa0");
 
 transition();
