55f5334524f82e1f9568c57d9d86bba9665a3956
LeFal <qwertyhj2@gmail.com>
2017-09-16 10:58:28 +0900

    add ARKit

--- BaekYangLi/BaekYangLi/ViewControllers/ARViewController.swift
+++ BaekYangLi/BaekYangLi/ViewControllers/ARViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017년 BaekYangLi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ARCL
+import CoreLocation
+
+class ARViewController: UIViewController {
+    var sceneLocationView = SceneLocationView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneLocationView.run()
+        view.addSubview(sceneLocationView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = view.bounds
+    }
+
+}
+
