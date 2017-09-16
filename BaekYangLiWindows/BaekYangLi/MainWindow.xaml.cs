daaacde91510bd152e9153cdd17ef8abb75f3519
Lee-WonJun <dldnjs1013@nate.com>
2017-09-16 23:50:27 +0900

    get position

--- BaekYangLiWindows/BaekYangLi/MainWindow.xaml.cs
+++ BaekYangLiWindows/BaekYangLi/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Device.Location;
 using System.Windows.Shapes;
 
 namespace BaekYangLi
@@ -20,9 +21,35 @@ namespace BaekYangLi
     /// </summary>
     public partial class MainWindow : Window
     {
+        GeoCoordinateWatcher GW = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+        
         public MainWindow()
         {
             InitializeComponent();
+            GetGPS();
+        }
+
+        public void GetGPS()
+        {
+            bool started = GW.TryStart(true, TimeSpan.FromMilliseconds(2000));
+            if (!started)
+            {
+                Console.WriteLine("GeoCoordinateWatcher timed out on start.");
+            }
+
+            GW.PositionChanged += PositionChanged;
+        }
+
+        private void PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            if (e.Position.Location != null)
+            {
+                Console.WriteLine("Lat: {0}, Long: {1}",
+                    e.Position.Location.Latitude,
+                    e.Position.Location.Longitude);
+                GW.Stop();
+            }
+
         }
     }
 }
