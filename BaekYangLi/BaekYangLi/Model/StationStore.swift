7d709490f27974bff523dd1e28b42448f0f2d4e5
sohn126 <nhos126@gmail.com>
2017-09-16 20:07:09 +0900

    remove MetroStore

--- BaekYangLi/BaekYangLi/Model/StationStore.swift
+++ BaekYangLi/BaekYangLi/Model/StationStore.swift
@@ -13,6 +13,7 @@ class StationStore {
     static let shared = StationStore()
     
     private(set) var nearestStations: [Station] = []
+    private(set) var groupedStations: [[Station]] = []
     
     public func getNearestStations(completion: @escaping () -> ()) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
@@ -22,6 +23,27 @@ class StationStore {
             self.nearestStations.sort(by: { (first, second) -> Bool in
                 return first.distance! < second.distance!
             })
+            
+//            var stationGroup = [Station]()
+//            var previousStationName: String = ""
+//            for station in nearestStations {
+//                var stationGroup = [Station]()
+//                if station.name == previousStationName {
+//                    stationGroup.append(station)
+//                } else {
+//                    groupedStations.append(stationGroup)
+//                }
+//                
+//                
+//                previousStationName = station.name
+//                
+//            }
+//            while groupedStations.count < 3 {
+//                
+//                
+//                
+//            }
+            
             
             for station in self.nearestStations {
                 print(station.name!)
